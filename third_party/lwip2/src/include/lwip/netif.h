089daa0ef5559faa96888c815ff3c20bd7c6da5b
Tobias Diedrich <ranma+git@tdiedrich.de>
2023-06-11 22:20:37 +0200

    Flag compatibility with LWIP1
    cnx_sta_leave looks at NETIF_FLAG_DHCP (0x08) to decide if DHCP should be
    stopped or not.
    
    wifi_station_dhcpc_start and wifi_set_ip_info look at NETIF_FLAG_UP
    (0x01).

--- third_party/lwip2/src/include/lwip/netif.h
+++ third_party/lwip2/src/include/lwip/netif.h
@@ -85,26 +85,29 @@ extern "C" {
 /** If set, the netif has broadcast capability.
  * Set by the netif driver in its init function. */
 #define NETIF_FLAG_BROADCAST    0x02U
+/** If set, the netif has MLD6 capability.
+ * Set by the netif driver in its init function. */
+#define NETIF_FLAG_MLD6         0x04U
+/** If set, the interface is configured using DHCP.
+ * Set by the DHCP code when starting or stopping DHCP. */
+#define NETIF_FLAG_DHCP         0x08U
 /** If set, the interface has an active link
  *  (set by the network interface driver).
  * Either set by the netif driver in its init function (if the link
  * is up at that time) or at a later point once the link comes up
  * (if link detection is supported by the hardware). */
-#define NETIF_FLAG_LINK_UP      0x04U
+#define NETIF_FLAG_LINK_UP      0x10U
 /** If set, the netif is an ethernet device using ARP.
  * Set by the netif driver in its init function.
  * Used to check input packet types and use of DHCP. */
-#define NETIF_FLAG_ETHARP       0x08U
+#define NETIF_FLAG_ETHARP       0x20U
 /** If set, the netif is an ethernet device. It might not use
  * ARP or TCP/IP if it is used for PPPoE only.
  */
-#define NETIF_FLAG_ETHERNET     0x10U
+#define NETIF_FLAG_ETHERNET     0x40U
 /** If set, the netif has IGMP capability.
  * Set by the netif driver in its init function. */
-#define NETIF_FLAG_IGMP         0x20U
-/** If set, the netif has MLD6 capability.
- * Set by the netif driver in its init function. */
-#define NETIF_FLAG_MLD6         0x40U
+#define NETIF_FLAG_IGMP         0x80U
 
 /**
  * @}
