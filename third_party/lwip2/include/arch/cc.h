fd8bd2bb3bd12a62f5df5f591dc875be55b8c2b1
Tobias Diedrich <ranma+git@tdiedrich.de>
2023-06-18 23:50:46 +0200

    Enable LWIP2 MEMLEAK_DEBUG

--- third_party/lwip2/include/arch/cc.h
+++ third_party/lwip2/include/arch/cc.h
@@ -1,9 +1,12 @@
 #ifndef LWIP2_ARCH_CC_H
 #define LWIP2_ARCH_CC_H
 
+#define MEMLEAK_DEBUG 1
+
 #include <stdint.h>
 #include "c_types.h"
 #include "osapi.h"
+#include "mem.h"
 
 #define SYS_ARCH_DECL_PROTECT(lev) uint32_t lev
 #define SYS_ARCH_PROTECT(lev) do { __asm__ __volatile__("rsil %0, 3\n" : "=a"(lev) ::); } while (0)
@@ -16,5 +19,9 @@
 
 #define sys_jiffies sys_now
 #define printf os_printf
+
+#define mem_clib_malloc os_malloc
+#define mem_clib_calloc os_calloc
+#define mem_clib_free   os_free
 
 #endif /* LWIP2_ARCH_CC_H */
